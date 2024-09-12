a1e5826ec9cf8425aec8d4f8d76d0f788c58b726
starci183 <cuongnvtse160875@gmail.com>
2024-09-12 16:28:05 +0700

    hentai

--- Assets/CiFarm/Scripts/Services/NakamaServices/NakamaInitializerService.cs
+++ Assets/CiFarm/Scripts/Services/NakamaServices/NakamaInitializerService.cs
@@ -12,6 +12,7 @@ namespace CiFarm.Scripts.Services.NakamaServices
     public class NakamaInitializerService : ManualSingletonMono<NakamaInitializerService>
     {
         [Header("Nakama Config")]
+        [SerializeField] private bool useLocal = true;
         [SerializeField] private bool useHttps = true;
         [SerializeField] private string host = "api.cifarm-server.starci.net";
 
@@ -42,7 +43,14 @@ namespace CiFarm.Scripts.Services.NakamaServices
         {
 #if UNITY_EDITOR
             //set fake credentials for edtior only
-            SetEditorCredentials();
+            if (useLocal)
+            {
+                SetEditorLocalCredentials();
+            } else
+            {
+                SetEditorProductionCredentials();
+            }
+  
 #endif
             yield return new WaitUntil(() => credentials != null);
             //initialize
@@ -57,16 +65,27 @@ namespace CiFarm.Scripts.Services.NakamaServices
 
         }
 
-        private void SetEditorCredentials()
+        private void SetEditorLocalCredentials()
         {
             credentials = new Credentials()
             {
-                message = "6353a079-6288-405d-a92d-db75d080ca47",
+                message = "0d9c4c9f-e52e-4b00-8aaa-1e6caaa50890",
                 publicKey = "0x2a27cc686C4c00fAbAB169733E8f0A89a3e348bA",
-                signature = "0x5844831e2bd71f9a3f52197662e1a158c8bb731b942bd08fc3440ac60c146f7a54e857b03b33d392d9ce004710130263d7298ff5b747995e9a46fd4950901b791c",
+                signature = "0x3570775c081781655265d6784ef77535a04e161653917e98938f9503ffee5797220a8fbd6c46e0853fb4e5d3cb608af8b3be1b7397aa4c3cbda456e115ccc7251b",
                 chainKey = "avalanche"
             };
         }
+        private void SetEditorProductionCredentials()
+        {
+            credentials = new Credentials()
+            {
+                message = "1ccd5c84-93c9-4bb9-a40a-285b4c5405e2",
+                publicKey = "0x2a27cc686C4c00fAbAB169733E8f0A89a3e348bA",
+                signature = "0x2f2d66a0502b990f4e8450d54277cf4b7297d45b2e04bed2fcfc606fc2e1e72e3b72282f3a0798e45e4b1715bf90c3660eaa2bf0780bc1a9196e6468915d39a61c",
+                chainKey = "avalanche"
+            };
+        }
+
 
         //called from React app to set credentials, then break the coroutine
         public void SetCredentials(string payload)
@@ -77,13 +96,24 @@ namespace CiFarm.Scripts.Services.NakamaServices
         public void InitializeClient()
         {
             try
-            {   
-                var scheme = useHttps ? "https" : "http";
-                client = new Client(scheme, host, port, serverKey, UnityWebRequestAdapter.Instance)
+            {      
+                if (useLocal)
                 {
-                    Timeout                  = 5, //5s
-                    GlobalRetryConfiguration = new RetryConfiguration(1, 0)
-                };
+                    client = new Client("http", "localhost", 7350, "defaultkey", UnityWebRequestAdapter.Instance)
+                    {
+                        Timeout = 5, //5s
+                        GlobalRetryConfiguration = new RetryConfiguration(1, 0)
+                    };
+                } else
+                {
+                    var scheme = useHttps ? "https" : "http";
+                    client = new Client(scheme, host, port, serverKey, UnityWebRequestAdapter.Instance)
+                    {
+                        Timeout = 5, //5s
+                        GlobalRetryConfiguration = new RetryConfiguration(1, 0)
+                    };
+                }
+
             }
             catch (Exception e)
             {
