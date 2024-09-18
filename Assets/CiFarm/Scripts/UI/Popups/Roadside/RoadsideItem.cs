9936d09bc2efb9004e03312ed043b3c35c4cdfd9
thientm27 <Denkhotieu1@gmail.com>
2024-09-18 21:34:45 +0700

    Roadside item

--- Assets/CiFarm/Scripts/UI/Popups/Roadside/RoadsideItem.cs
+++ Assets/CiFarm/Scripts/UI/Popups/Roadside/RoadsideItem.cs
@@ -1,7 +1,30 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace CiFarm.Scripts.UI.Popups.Roadside
 {
-    public class RoadsideItem
+    public class RoadsideItem : MonoBehaviour
     {
-        
+        [SerializeField] private TextMeshProUGUI textQuantity;
+        [SerializeField] private GameObject      plusButton;
+        [SerializeField] private GameObject      productGroup;
+        [SerializeField] private Image           productRender;
+
+        public void SetProductOnSale(Sprite sprite = null, int quantity = 0)
+        {
+            if (sprite != null && quantity != 0)
+            {
+                productGroup.SetActive(true);
+                plusButton.SetActive(false);
+                productRender.sprite = sprite;
+                textQuantity.text    = quantity.ToString();
+            }
+            else
+            {
+                productGroup.SetActive(false);
+                plusButton.SetActive(true);
+            }
+        }
     }
 }
--- Assets/CiFarm/Scripts/UI/Popups/RoadsideShopPopup.cs
+++ Assets/CiFarm/Scripts/UI/Popups/RoadsideShopPopup.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using CiFarm.Scripts.UI.Popups.Roadside;
 using Imba.UI;
 using UnityEngine;
 
@@ -5,6 +7,30 @@ namespace CiFarm.Scripts.UI.Popups
 {
     public class RoadsideShopPopup : UIPopup
     {
-       
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+
+        protected override void OnShowing()
+        {
+            base.OnShowing();
+        }
+
+        [SerializeField] private List<RoadsideItem> roadsideItems;
+
+        public void LoadItemsOnSale()
+        {
+            
+        }
+        public void OnPutNewItemOnSale(int index)
+        {
+        }
+
+        public void OnClickToRemoveItemOnSale(int index)
+        {
+        }
+        
+        
     }
 }
